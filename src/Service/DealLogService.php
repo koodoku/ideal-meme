af077ae6683c973b7cd9e0295e7ca218917f3298
Lera Ershova <ersovavaleria215@gmail.com>
2025-06-05 13:21:13 +1000

    fix delta

--- src/Service/DealLogService.php
+++ src/Service/DealLogService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Entity\Application;
 use App\Entity\DealLog;
+use App\Entity\Depositary;
 use App\Enums\ActionEnum;
 use App\Repository\DealLogRepository;
 
@@ -25,10 +26,47 @@ class DealLogService
             ->setPrice($buyApplication->getPrice()) // min($buyApplication->getPrice(), $sellApplication->getPrice()) для "комплесных" сделок
             ->setBuyPortfolio($buyApplication->getPortfolio())
             ->setSellPortfolio($sellApplication->getPortfolio())
+            ->setQuantity($buyApplication->getQuantity()) // min($buyApplication->getQuantity(), $sellApplication->getQuantity()) для "комплесных" сделок
         ;
 
         $this->dealLogRepository->saveDealLog($dealLog);
 
         return $dealLog;
+    }
+
+    public function calculateDelta(Depositary $depositary): float
+    {
+        $sellDealLogs =
+            $depositary->getPortfolio()->getSellDealLogs()->filter(
+                function (DealLog $sellDealLog) use ($depositary) {
+                    return $depositary->getStock()->getId() === $sellDealLog->getStock()->getId();
+                }
+            );
+
+        $buyDealLogs =
+            $depositary->getPortfolio()->getBuyDealLogs()->filter(
+                function (DealLog $buyDealLog) use ($depositary) {
+                    return $depositary->getStock()->getId() === $buyDealLog->getStock()->getId();
+                }
+            );
+
+        $latestDealLog = $this->dealLogRepository->findLatestByStock($depositary->getStock());
+
+        $investSum = 0.0;
+        $actualQuantity = 0;
+
+        foreach ($buyDealLogs as $buyDealLog) {
+            $investSum += $buyDealLog->getQuantity() * $buyDealLog->getPrice();
+            $actualQuantity += $buyDealLog->getQuantity();
+        }
+
+        foreach ($sellDealLogs as $sellDealLog) {
+            $investSum -= $sellDealLog->getQuantity() * $sellDealLog->getPrice();
+            $actualQuantity -= $sellDealLog->getQuantity();
+        }
+
+        $actualSum = $actualQuantity * ($latestDealLog?->getPrice() ?? 0.0);
+
+        return $actualSum - $investSum;
     }
 }
